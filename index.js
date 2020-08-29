a03d348b4552ac6daa7769141761301976b7971f
ashirafumiiro <ashirafumiiro@gmail.com>
2020-08-29 14:30:23 +0300

    fixing port

--- index.js
+++ index.js
@@ -99,7 +99,8 @@ app.post('/api/persons', (request, response)=>{
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
-app.listen(PORT)
-console.log(`Server listening at port: ${PORT}`);
+app.listen(PORT, ()=>{
+    console.log(`Server listening at port: ${PORT}`);
+})
